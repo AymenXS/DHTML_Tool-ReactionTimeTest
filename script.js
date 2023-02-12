5e639367d13d40546114309a733ce50d40547808
AymenXS <aymenghaloua@gmail.com>
2023-02-12 06:41:10 +0100

    Wrapping Up

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ canvas.width = parentWidth;
 
 context.textAlign = "center";
 context.textBaseline = "middle";
-context.fillText("Click in this area", canvas.height / 2, canvas.width / 2);
+context.fillText("Click in this area", canvas.width / 2, canvas.height / 2);
 
 let start = document.getElementById("start"),
   timeText = document.getElementById("time-text");
@@ -22,19 +22,46 @@ let gameStatus = {
 let status = gameStatus.STOP;
 
 function getRandomTime(min, max) {
-  let result = Math.floor(Math.random() * Math.floor(max)) + 1;
+  let result = Math.floor(Math.random() * Math.floor(max)) + min;
   result = result * 1000;
-  console.log(result);
   return result;
 }
 
-function endGame() {}
+function endGame() {
+  clearTimeout(timeout1);
+  clearTimeout(timeout2);
+  canvas.style.background = "rgb(237,255,172)";
+  start.innerHTML = "Start Game";
+  status = gameStatus.STOP;
+}
+
+function timeoutFunction1(time) {
+  timeout1 = setTimeout(function () {
+    canvas.style.background = "rgb(78,197,78)";
+    let date1 = new Date();
+    timeNow = date1.getTime();
+    canvas.addEventListener("click", function () {
+      let date2 = new Date();
+      timeLater = date2.getTime();
+      playTime = timeLater - timeNow;
+      timeText.innerHTML = playTime + " ms";
+    });
+  }, time);
+}
+
+function timeoutFunction2(time) {
+  timeout2 = setTimeout(function () {
+    endGame();
+  }, time);
+}
 
 function startGame() {
   let changeTime = getRandomTime(1, 8);
   let endTime = changeTime + 5000;
   status = gameStatus.START;
   canvas.style.background = "rgb(206,63,63)";
+  timeoutFunction1(changeTime);
+  timeoutFunction2(endTime);
 }
 
 start.addEventListener("click", function () {
@@ -44,4 +71,8 @@ start.addEventListener("click", function () {
     startGame();
     this.innerHTML = "Stop Game";
   }
+});
+
+canvas.addEventListener("click", function () {
+  endGame();
 });
